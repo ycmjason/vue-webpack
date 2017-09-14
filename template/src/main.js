5377f4b25371136090e9afaa507a2859bd91eb74
Jason Yu <me@ycmjason.com>
2017-09-14 15:41:16 +0800

    fix Vue.use call order

--- template/src/main.js
+++ template/src/main.js
@@ -1,13 +1,9 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Vuex from 'vuex';
 
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
 
-Vue.use(VueRouter);
-Vue.use(Vuex);
 
 new Vue({
   router,
